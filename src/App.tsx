080e04fbdbde30dd00216c987e95fa84c7c7af4e
tuliuxiang <wangyitt13213@163.com>
2024-12-28 18:37:52 +0800

    feat: 接口连接测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import type { UserItem } from "./type";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userList, setUserList] = useState<UserItem[]>([]);
+
+  const getUserList = () => {
+    fetch("http://111.229.142.50:9981/admin/ctr/user")
+      // fetch("http://localhost:9981/admin/ctr/user")
+      .then((res) => res.json())
+      .then((res) => {
+        setUserList(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getUserList();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <React.Fragment>
+      {userList.map((item, index) => {
+        return (
+          <div key={index}>
+            用户：<span>{item.name}</span>
+            ，添加时间：<span>{item.addTime}</span>
+          </div>
+        );
+      })}
+      <p className="read-the-docs">Click on</p>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
---
+++ src/type.ts
@@ -0,0 +1,8 @@
+export interface UserItem {
+  id: number;
+  name: string;
+  age: number;
+  email: string;
+  addTime: string;
+  description: string;
+}
